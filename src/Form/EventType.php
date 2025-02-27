40ba728c1bf9c61b9883330e0dd9f1458cf48584
Garance <davidgarance0@gmail.com>
2025-02-27 14:53:26 +0100

    front détail évenement + mise à jours EventController.php

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -10,38 +10,4 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class EventType extends AbstractType
 {
 
-//    public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options) : void
-//    {
-//        $builder
-//            ->add('campus', ChoiceType::class, [
-//                'label' => 'Site :',
-//                'choices' => [
-//                    'Nantes',
-//                    'Rennes',
-//                    'Quimmper',
-//                    'Niort',
-//                    'En ligne',
-//                ]
-//            ])
-//            ->add('event', EntityType::class, [
-//                'attr' => ['placeholder' => 'search'],
-//                'label' => 'Le nom de la sortie contient',
-//            ])
-//            ->add('date',   DateType::class, [])
-//            ->add ('event_checkb', CheckboxType::class, [
-//                'expanded' => true,
-//                'multiple' => true,
-//
-//            ]);
-//
-//    }
-//    public function configureOptions(OptionsResolver $resolver) : void {
-//
-//        $resolver->setDefaults([
-//            'data_class' => \App\Entity\Event::class,
-//            'method' => 'GET',
-//            // 'csrf_protection' => false
-//        ]);
-//    }
-
 }
